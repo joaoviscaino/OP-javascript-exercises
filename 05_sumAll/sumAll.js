2dc18e80b561cd6753c61b1b00aba8ffda9b691f
joaoviscaino <joao-viscaino@hotmail.com>
2024-10-07 22:58:14 -0400

    Finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    /* for loop that will run the amount of numbers setting them apart 
+        Sum begins with x  
+                            */
+    if (x < 0 || y < 0 || typeof x !== 'number' || typeof y !== 'number'
+        || !Number.isInteger(x) || !Number.isInteger(y)
+    ) return 'ERROR';
+    if (x > y) {
+        //x = [y, y = x][0]; // I cannot believe this is allowed.
+        // this is more readable. The above expression is WILD and does all sorts of things in one line, wth
+        [x, y] = [y, x];
+    }
+        for (let i = x; i <= y; i++) {
+            sum += i;
+        }
+    return sum;
 };
 
 // Do not edit below this line
