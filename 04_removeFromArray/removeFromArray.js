46de92fc27fe48171d19dac45f93be5b0cc56d13
joaoviscaino <joao-viscaino@hotmail.com>
2024-10-07 22:02:39 -0400

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+/*
+const removeFromArray = function(arr, ...elements) {
+    elements.forEach(element => {
+        // where were that element in arr, as index
+        const index = arr.indexOf(element);
+        if (index > -1)
+            arr.splice(index, 1); 
+    });
+    return arr;
+};  
+*/
+
+/* WE COULD ALSO USE THE FILTER METHOD */
+
+const removeFromArray = function(arr, ...elements) {    
+    return arr = arr.filter(x => !elements.includes(x));
 };
 
 // Do not edit below this line
